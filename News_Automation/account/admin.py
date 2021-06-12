b3e4960431d77de046e20742078089e0d72e69cf
manikantapabba <60176870+manikantapabba@users.noreply.github.com>
2021-06-12 20:17:32 +0530

    Delete News_Automation directory

--- News_Automation/account/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from account.models import MyUser
-
-admin.site.register(MyUser)
--- News_Automation/news/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import News_Update,Prefer
-
-# Register your models here.
-admin.site.register(News_Update)
-admin.site.register(Prefer)
